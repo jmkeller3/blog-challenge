ab226757ff1147ca9af50667b28d9d5f69e97c27
John Keller <jmkeller3@gmail.com>
2018-06-13 15:31:33 -0500

    test 1

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const morgan = require('morgan');
 
-const {Blogs} = require('./models')
+const bodyParser = require('body-parser');
+
+const {Blogs} = require('./models');
+
+const jsonParer = bodyParser.json();
+const app = express();
+
+app.use(morgan('common'));
